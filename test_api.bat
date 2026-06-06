7a6f959def4f53145a546ee4eb01ea7ae27faac0
rsh-119 <personalrsh11@gmail.com>
2026-06-06 22:21:27 +0530

    restart

--- test_api.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Testing Fraud Detection API...
-echo.
-curl -X POST https://fraud-detection-using-ml.onrender.com/predict -H "X-API-Key: prod_key_123" -H "Content-Type: application/json" -d "{\"TransactionAmt\":1500,\"ProductCD\":\"W\",\"card1\":12345}"
-echo.
-pause
